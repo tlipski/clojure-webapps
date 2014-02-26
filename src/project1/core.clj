62457ef3f9cca35b36ece08cc56cada53da5aee5
Tomek Lipski <tomek.lipski@gmail.com>
2014-02-26 23:24:09 +0100

    section1 video3

--- src/project1/core.clj
+++ src/project1/core.clj
@@ -1,5 +1,15 @@
 (ns project1.core)
 
+(defn example-handler [request]
+ {:body "Hello Clojure!"}
+ )
+
+(defn on-init []
+ (println "Initializing sample webapp!"))
+
+(defn on-destroy []
+ (println "Destroying sample webapp!"))
+
 (defn foo
   "I don't do a whole lot."
   [x]
