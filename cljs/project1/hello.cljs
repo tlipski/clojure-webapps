9e4ca9808fbea11265f6700a45327cbc4ad21d8d
Tomek Lipski <tomek.lipski@gmail.com>
2014-04-12 00:09:26 +0200

    section 7 video 3

--- cljs/project1/hello.cljs
+++ cljs/project1/hello.cljs
@@ -1,9 +1,29 @@
 (ns project1.hello)
 
-(let [el (.createElement js/document "a")
-      txt (.createTextNode js/document "Hello world!")]
- (.appendChild el txt)
- (.setAttribute el "href" "#clojure")
- (.addEventListener el "click"
-		    (fn [] (js/alert (str "Clicked: " (.-outerHTML el)))))
- (.appendChild (.getElementById js/document "holder") el))
+(defn show-entries [entries]
+  (let [ul-el (.getElementById js/document "entries")]
+    (aset ul-el "innerHTML" ""); clean the document
+    (doseq [entry entries]
+     (let [li-el (.createElement js/document "li")]
+       (.appendChild li-el (.createTextNode js/document (get (js->clj entry) "title")))
+       (.appendChild ul-el li-el)))))
+
+(defn load-entries []
+ (let [req (js/XMLHttpRequest.)]
+  (aset req "onreadystatechange"
+	(fn []
+		(when (= (.-readyState req) 4)
+			(show-entries (JSON/parse (.-responseText req))))))
+  (.open req "GET" "/entries" true)
+  (.send req "")))
+
+(defn ^:export add-entry []
+ (let [title (.-value (.getElementById js/document "title"))
+       req (js/XMLHttpRequest.)]
+  (aset req "onreadystatechange" 
+	(fn [] (when (= (.-readyState req) 4) (load-entries))))
+  (.open req "POST" "/entries" true)
+  (.setRequestHeader req "Content-type" "application/json")
+  (.send req (.stringify js/JSON (clj->js {:title title})))))
+
+(load-entries)
