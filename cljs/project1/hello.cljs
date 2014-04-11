c551aac999203f1e24761a824be543f9cbc39baa
Tomek Lipski <tomek.lipski@gmail.com>
2014-04-11 23:37:59 +0200

    section 7 video 2

--- cljs/project1/hello.cljs
+++ cljs/project1/hello.cljs
@@ -1,4 +1,9 @@
 (ns project1.hello)
 
-(js/alert "Hello ClojureScript!")
-
+(let [el (.createElement js/document "a")
+      txt (.createTextNode js/document "Hello world!")]
+ (.appendChild el txt)
+ (.setAttribute el "href" "#clojure")
+ (.addEventListener el "click"
+		    (fn [] (js/alert (str "Clicked: " (.-outerHTML el)))))
+ (.appendChild (.getElementById js/document "holder") el))
